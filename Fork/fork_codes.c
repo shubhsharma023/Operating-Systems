8e9f4ca026e217902ea131377bc7867bd2d93701
Shubhankar Sharma <97976855+shubhsharma023@users.noreply.github.com>
2023-04-05 08:25:36 +0530

    Fork initial commit
    types of fork implementation

---
+++ Fork/fork_codes.c
@@ -0,0 +1,89 @@
+/*SHUBHANKAR SHARMA  102118021  COBS1*/
+
+#include<stdio.h>
+#include<sys/types.h> /*sys/types.h provides definitions for system calls,*/
+#include<unistd.h> /*used for unix-like os*/
+int main(){
+
+
+// // 1.  if    fork                                   
+// if (fork())
+// {
+//     fork();
+//     fork();
+//     fork();
+//     printf("Hello!\n");
+// }printf("Hi!\n");
+// return 0;
+
+    // fork();
+    // fork();
+    // fork();
+    // printf("Hello!\n");
+    // return 0;
+
+
+// // 2. fork OR fork
+// if (fork() || fork())
+//     {
+//         printf("Hello!\n");
+//     }printf("Hi!\n");
+//     return 0;
+
+
+// // 3. fork AND fork
+//    if (fork() && fork())
+//     {
+//         printf("Hello!\n");
+//     }printf("Hi!\n");
+//     return 0;
+
+
+// // 4. fork AND fork AND fork
+    // if (fork() && fork()&&fork())
+    // {
+    //     printf("Hello!\n");
+    // }printf("Hi!\n");
+    // return 0;
+
+
+// // 5. fork OR fork OR fork
+    // if (fork() || fork() || fork())
+    // {
+    //     printf("Hello!\n");
+    // }printf("Hi!\n");
+    // return 0;
+
+
+// // 6. fork AND fork OR fork
+//  fork()&&fork()||fork();
+//  printf("Hello!\n");
+//  return 0;      
+
+
+// 7. fork OR fork AND fork
+    //  fork()||fork()&&fork();
+    //  printf("Hello!\n");
+    //  return 0;   
+
+
+// // 8. if fork fork OR fork
+// if (fork()){
+//     fork()||fork();
+//     printf("Hello!\n");
+// }
+// else printf("Hi!\n");
+// return 0;
+
+
+// // 9. if fork fork AND fork
+// if (fork()){
+//     fork()&&fork();
+//     printf("Hello!\n");
+// }
+// else printf("Hi!\n");
+// return 0;
+
+
+
+}
